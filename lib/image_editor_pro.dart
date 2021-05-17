2cc73895fedbb294fc9de95f75b976a72539c66f
jahuanca <joan.huanca19@gmail.com>
2021-05-16 21:33:52 -0500

    valor

--- lib/image_editor_pro.dart
+++ lib/image_editor_pro.dart
@@ -58,7 +58,7 @@ class _ImageEditorProState extends State<ImageEditorPro> {
   Offset offset1 = Offset.zero;
   Offset offset2 = Offset.zero;
   final scaf = GlobalKey<ScaffoldState>();
-  var openbottomsheet = false;
+  var openbottomsheet = true;
   List<Offset> _points = <Offset>[];
   List type = [];
   List aligment = [];
@@ -92,7 +92,7 @@ class _ImageEditorProState extends State<ImageEditorPro> {
     multiwidget.clear();
     howmuchwidgetis = 0;
     // TODO: implement initState
-    bottomsheets();
+                
     super.initState();
   }
 
